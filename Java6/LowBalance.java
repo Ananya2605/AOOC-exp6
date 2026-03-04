16af5432ba1f1cda34f11ba37e47f5628c23633c
Ananya2605 <ananyasutar7045@gmail.com>
2026-03-04 22:14:04 +0530

    Add files via upload

---
+++ Java6/LowBalance.java
@@ -0,0 +1,60 @@
+
+class LowBalanceException extends Exception {
+    public LowBalanceException(String message) {
+        super(message);
+    }
+}
+
+
+class NegativeNumberException extends Exception {
+    public NegativeNumberException(String message) {
+        super(message);
+    }
+}
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void balanceEnquiry() {
+        System.out.println("Current Balance: $" + balance);
+    }
+
+    public void deposit(double amount) throws NegativeNumberException {
+        if (amount < 0) {
+            throw new NegativeNumberException("Cannot deposit a negative amount.");
+        }
+        balance += amount;
+        System.out.println("Successfully deposited: $" + amount);
+    }
+
+    public void withdraw(double amount) throws LowBalanceException, NegativeNumberException {
+        if (amount < 0) {
+            throw new NegativeNumberException("Cannot withdraw a negative amount.");
+        }
+        if (amount > balance) {
+            throw new LowBalanceException("Insufficient funds! Current balance: $" + balance);
+        }
+        balance -= amount;
+        System.out.println("Successfully withdrew: $" + amount);
+    }
+}
+
+public class LowBalance {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(500.0);
+
+        try {
+            account.balanceEnquiry();
+            account.deposit(200);
+            account.withdraw(800); 
+        } catch (LowBalanceException | NegativeNumberException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            System.out.println("Transaction attempt finished.");
+        }
+    }
+}
---
+++ Java6/Oddnum.java
@@ -0,0 +1,26 @@
+public class OddNumberException extends Exception {
+    public OddNumberException(String message) {
+        super(message);
+    }
+}
+
+public class Oddnum {
+    public static void checkEven(int number) throws OddNumberException {
+        if (number % 2 != 0) {
+            throw new OddNumberException(number + " is an odd number!");
+        }
+        System.out.println(number + " is even. Proceeding...");
+    }
+
+    public static void main(String[] args) {
+        int testValue = 7;
+        try {
+            checkEven(testValue);
+        } catch (OddNumberException e) {
+            System.out.println("Caught Exception: " + e.getMessage());
+        }
+    }
+}
+ {
+    
+}
